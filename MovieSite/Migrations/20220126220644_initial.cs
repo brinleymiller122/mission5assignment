60c9920f08fc560afa9940f238006bac6fe1378d
brinleymiller122 <miller.brinley@gmail.com>
2022-01-26 15:13:47 -0700

    Add project files.

---
+++ MovieSite/Migrations/20220126220644_initial.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MovieSite.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Responses",
+                columns: table => new
+                {
+                    MovieId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(nullable: false),
+                    Year = table.Column<int>(nullable: false),
+                    Director = table.Column<string>(nullable: false),
+                    Rating = table.Column<string>(nullable: false),
+                    Edited = table.Column<bool>(nullable: false),
+                    LentTo = table.Column<string>(nullable: true),
+                    Notes = table.Column<string>(maxLength: 25, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Responses", x => x.MovieId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Responses",
+                columns: new[] { "MovieId", "Director", "Edited", "LentTo", "Notes", "Rating", "Title", "Year" },
+                values: new object[] { 1, "Shawn Levy", false, null, "Ryan Reynolds is the best ever...", "PG", "Free Guy", 2021 });
+
+            migrationBuilder.InsertData(
+                table: "Responses",
+                columns: new[] { "MovieId", "Director", "Edited", "LentTo", "Notes", "Rating", "Title", "Year" },
+                values: new object[] { 2, "Taika Waititi", false, null, "Powerful", "PG-13", "JoJo Rabbit", 2019 });
+
+            migrationBuilder.InsertData(
+                table: "Responses",
+                columns: new[] { "MovieId", "Director", "Edited", "LentTo", "Notes", "Rating", "Title", "Year" },
+                values: new object[] { 3, "Jared Hess", false, null, "Hilarious", "PG", "Napoleon Dynamite", 2004 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Responses");
+        }
+    }
+}
---
+++ MovieSite/Models/MovieFormsContext.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieSite.Models
+{
+    public class MovieFormsContext : DbContext
+    {
+        public MovieFormsContext(DbContextOptions<MovieFormsContext> options) : base (options)
+        {
+
+        }
+
+        public DbSet<MovieModel> Responses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<MovieModel>().HasData(
+                new MovieModel
+                {
+                    MovieId = 1,
+                    Title = "Free Guy",
+                    Year = 2021,
+                    Director = "Shawn Levy",
+                    Rating = "PG",
+                    Notes = "Ryan Reynolds is the best ever..."
+
+                },
+                new MovieModel
+                {
+                    MovieId = 2,
+                    Title = "JoJo Rabbit",
+                    Year = 2019,
+                    Director = "Taika Waititi",
+                    Rating = "PG-13",
+                    Notes = "Powerful"
+                },
+                new MovieModel
+                {
+                    MovieId = 3,
+                    Title = "Napoleon Dynamite",
+                    Year = 2004,
+                    Director = "Jared Hess",
+                    Rating = "PG",
+                    Notes = "Hilarious"
+                }
+                ) ;
+        }
+    }
+}
